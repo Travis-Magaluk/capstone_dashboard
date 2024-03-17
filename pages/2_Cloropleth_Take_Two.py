2ecc4a189d43ec1158e4542a9ccb84414be9c6be
Travis Magaluk <twmagalu@gmail.com>
2024-03-17 12:11:03 -0500

    Testing Stacked static plots

--- pages/2_Cloropleth_Take_Two.py
+++ pages/2_Cloropleth_Take_Two.py
@@ -3,8 +3,9 @@ import pandas as pd
 import sqlalchemy
 import geopandas as gpd
 import psycopg2
-
+import matplotlib.pyplot as plt
 import plotly.express as px
+
 conn = st.connection("postgresql", type="sql")
 
 # Perform query.
@@ -13,34 +14,23 @@ df = conn.query('SELECT * FROM credentials;', ttl="10m")
 st.title("Choropleth Map")
 st.header("Working on displaying different data through a Choropleth Map")
 
-connection = psycopg2.connect(database = 'dentdb',
-                              user = df.iloc[0][3],
-                              host = df.iloc[0][1],
-                              password = df.iloc[0][4])
-
+connection = psycopg2.connect(database='dentdb',
+                              user=df.iloc[0][3],
+                              host=df.iloc[0][1],
+                              password=df.iloc[0][4])
 
 SQL = "SELECT * FROM travis_testing;"
-
-mo_counties = gpd.read_postgis(SQL,connection)
-
+SQL2 = "SELECT * FROM all_providers_dist"
+moves = gpd.read_postgis(SQL2, connection)
+mo_counties = gpd.read_postgis(SQL, connection)
+st.write(len(moves))
 mo_counties['dhyg_cnt_trct_year_hyg_count'] = mo_counties['dhyg_cnt_trct_year_hyg_count'].fillna(0)
-
-fig = px.choropleth(mo_counties,
-                   geojson=mo_counties.geom,
-                   locations=mo_counties.index,
-                   projection="mercator",
-                    color='dhyg_cnt_trct_year_hyg_count',
-                    labels={'dhyg_cnt_trct_year_hyg_count':'Hygienists in 2020'})
-fig.update_layout(
-    autosize=False,
-    width=800,
-    height=800,
-)
-fig.update_geos(fitbounds="locations", visible=False)
-st.plotly_chart(fig, use_container_width=True)
-
-df = px.data.carshare()
-fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon", color="peak_hour", size="car_hours",
-                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
-                  mapbox_style="carto-positron")
-st.plotly_chart(fig)
+moves = moves[moves['lic_exp_da'] == '11/30/2022']
+moves = moves[moves['lic_profes'] == 'DEN']
+st.write(len(moves))
+st.header('Census Tract Population Choropleth Map')
+fig, ax = plt.subplots(figsize=(10, 8))
+mo_counties.plot(column='dhyg_cnt_trct_year_hyg_count', cmap='Blues', ax=ax, legend=True)
+moves.plot(ax=ax, color='red', markersize=3, label='Dental Providers', alpha=0.2)
+ax.set_title('Census Tract Provider Counts')
+st.pyplot(fig)
