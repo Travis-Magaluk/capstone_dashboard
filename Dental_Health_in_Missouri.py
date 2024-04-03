0c5d0149ce93427778c10c378738671c20d1d9c5
Travis Magaluk <twmagalu@gmail.com>
2024-04-03 17:51:47 -0400

    Revisioned first page and experimented with columns

--- Dental_Health_in_Missouri.py
+++ Dental_Health_in_Missouri.py
@@ -1,6 +1,48 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 
-st.title("Dental Provider Access in Missouri")
+st.title("Rural Access to Dental Services")
+st.markdown("#")
 
-st.write("""Some text about our dashbaord and what we want it to say....
-""")
+
+
+
+problem = """
+Many people in missouri are lacking adequate access to dental care. This is what some people have to say about 
+their experiences trying to get dental work done in the state. 
+
+- "For my middle girl, McKenzie, she ended up having to have a root canal and that was a day out of school after having 
+three appointments to try and find someone," McKensay Vandelicht said.
+- "Taking the time off of work to have to drive further away because we don't have enough 
+dentists in the area to accommodate everyone's needs," Vandelicht said.  
+- "Forty-two percent of our patients are driving from outside the Columbia area to get work done, 
+because they don't have the dental care that they need provided in the smaller cities that they are from," 
+Parrott, who owns Aspen Dental in Columbia and Jefferson City, said.  
+- "Increasing the funding for loan repayment to help areas of decreased access would be a huge help," 
+Jacqueline Miller, director of Missouri Oral Health said.
+"""
+
+goals = """
+- Understand locations of dental providers across the state
+- Understand how dental providers move across the state. 
+- Deliver data driven guidance for future policy 
+"""
+
+with st.container():
+    col1, col2, col3 = st.columns(spec=[0.45, 0.1, 0.45])
+
+    with col1:
+       st.header("The Problem")
+       st.markdown(problem)
+
+    with col2:
+       st.header('')
+
+    with col3:
+       st.header("Goals")
+       st.markdown(goals)
+
+    st.write("\n\n\n")
+
+with st.container():
+    st.header("How to use this dashboard")
---
+++ pages/5_In_State_Moves_EDA.py
@@ -0,0 +1,83 @@
+import streamlit as st
+
+import plotly.express as px
+st.set_page_config(layout="wide")
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM all_moves;', ttl="10m")
+df.year_move = df.year_move.astype(str)
+
+if 'p_type' not in st.session_state:
+    st.session_state['p_type'] = df.lic_type.unique()
+    st.session_state.year_move = df.year_move.unique()
+    st.session_state.type_moves = df.type_moves.unique()
+    st.session_state.dist_vals = (0, 100)
+
+
+### How can I make this more reusable? Filtering params as a dict with key value pairs.
+## Is there a good pandas filtering function where I can input multiple params.
+
+def filter_df(dataframe, year_vals, provider_types, type_moves, dist_vals):
+    dataframe = dataframe[dataframe['year_move'].isin(year_vals)]
+    dataframe = dataframe[dataframe['lic_type'].isin(provider_types)]
+    dataframe = dataframe[dataframe['type_moves'].isin(type_moves)]
+    dataframe = dataframe[dataframe['distance_m'] < st.session_state.dist_vals[1]]
+    dataframe = dataframe[dataframe['distance_m'] > st.session_state.dist_vals[0]]
+    return dataframe
+
+
+def histplot(df):
+    hist_col = st.selectbox("Select x-axis data", options=other_cols, key=8)
+    if hist_col in continuous_cols:
+        nbins = st.slider("nbins", min_value=5, max_value=30)
+    else:
+        nbins = None
+    st.markdown("Some text about the selection above.")
+
+    plot = px.histogram(df, x=hist_col, nbins=nbins)
+    st.plotly_chart(plot)
+
+
+def boxplot(df):
+    x_axis_label = st.selectbox("Select x-axis data", options=other_cols, key=11)
+
+    st.markdown("Click on the legend to (add/remove makes)")
+
+    plot = px.box(df, x=x_axis_label, y='distance_m')
+    st.plotly_chart(plot)
+
+plot_options = ["Histogram", "Boxplot"]
+continuous_cols = ["distance_m"]
+other_cols = ['lic_type', 'year_move', 'type_moves', 'move_numbe']
+tab2, tab3 = st.tabs(plot_options)
+
+
+with st.container():
+    with st.expander('Filtering Options'):
+        with st.form(key='barchart_params'):
+            st.session_state.p_type = st.multiselect('Provider Type',
+                                                     df.lic_type.unique(), st.session_state.p_type)
+            st.session_state.year_move = st.multiselect('Year Move', df.year_move.unique(), st.session_state.year_move)
+            st.session_state.type_moves = st.multiselect('Type of Move', df.type_moves.unique(), st.session_state.type_moves)
+            st.session_state.dist_vals = st.slider(
+                'What distances of Moves do you want to see data on?',
+                0.0, float(df.distance_m.max()), (0.0, float(df.distance_m.max())))
+
+            st.form_submit_button(label='Update Dataframe')
+
+        filtered_df = filter_df(df, st.session_state.year_move,
+                                st.session_state.p_type, st.session_state.type_moves,
+                                st.session_state.dist_vals)
+
+        st.write(len(filtered_df))
+
+with st.container():
+    with tab2:
+        histplot(filtered_df)
+
+    with tab3:
+        boxplot(filtered_df)
+
+
+
